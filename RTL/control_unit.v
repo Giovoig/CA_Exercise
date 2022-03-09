742003f744c4135d03bfb4751362ad74ce9dae24
Giovanni Capocotta <r0875167@stud-r8-5.esat.kuleuven.be>
2022-03-09 10:33:37 +0100

    Started modifying the CU

--- RTL/control_unit.v
+++ RTL/control_unit.v
@@ -30,6 +30,14 @@ module control_unit(
    //The behavior of the control unit can be found in Chapter 4, Figure 4.18
 
    always@(*)begin
+            alu_src   = 1'b0;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b1;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = R_TYPE_OPCODE;
+            jump      = 1'b0;
 
       case(opcode)
          ALU_R:begin
@@ -44,8 +52,29 @@ module control_unit(
          end
          
          // Declare the control signals for each one of the instructions here...
+	ALU_I: begin
 
-         default:begin
+	end
+
+	BRANCH_EQ: begin
+		branch = 1'b1;
+
+	end
+	
+	JUMP: begin
+		jump = 1'b1;
+	end
+
+	LOAD: begin
+		
+	end
+
+	STORE: begin
+
+	end
+
+
+        default:begin
             alu_src   = 1'b0;
             mem_2_reg = 1'b0;
             reg_write = 1'b0;
